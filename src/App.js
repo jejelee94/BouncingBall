e87665ba20e28cf824a2dfaa6f4debe8600d2c69
jejelee94 <jejelee98@gmail.com>
2021-10-29 15:37:29 +0900

    Title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div>TAB THE WALL</div>
+		</div>
+	);
 }
 
 export default App;
